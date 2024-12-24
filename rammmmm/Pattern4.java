42240907703f66666ad39996fe6d8b706b61cd65
ramtej473 <22bk1a6622@stpetershyd.com>
2024-12-24 14:46:53 +0530

    first commit

---
+++ rammmmm/Pattern4.java
@@ -0,0 +1,42 @@
+package rammmmm;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		/*
+		 * 	***************************
+		 *  *                         *
+		 *  *                         *
+		 *  *                         *
+		 *  *                         *
+		 *  *                         *
+		 *  *                         *
+		 *  *                         *
+		 *  *************************** 
+		 */
+		for(int rows=1;rows<=20;rows++) {
+			System.out.print("\t");
+			if(rows == 1 || rows == 20) {
+				for(int cols=1;cols<=35;cols++) {
+					System.out.print("*");
+				}
+			}
+			else {
+				for(int cols=1;cols<=35;cols++) {
+					if(cols== 1 || cols == 35) {
+						System.out.print("*");	
+					}
+					else {
+						System.out.print(" ");
+					}
+					
+				}
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+
+}
